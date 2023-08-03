82e258daa02cbeb0a89bc4c5e071d03f4a1de295
ApplePi313 <98051420+ApplePi313@users.noreply.github.com>
2023-08-03 12:25:24 -0400

    updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Discord-RPC
 the python program I use for my custom discord status
+
+# Requirements:
+- python3
+- pypresence (run pip install pypresence if you don't already have it installed)
+
+# Install
+- run git clone https://github.com/ApplePi313/Discord-RPC.git
+- to run, either execute run.sh or run python sys_monitor.py
+- if you want this to run on start, set run.sh to be run on start (for kde, this is System Settings > Startup and Shutdown > Autostart
--- sys_monitor.py
+++ sys_monitor.py
@@ -23,14 +23,14 @@ while True:  # The presence will stay on as long as the program is running
         print(RPC.update(large_image="arch-linux", large_text="Arch Linux",
                      small_image="i712thgen", small_text="Intel i7-12700H",
                      details="RAM: " + str(mem_per) + "%  CPU: " + str(cpu_per) + "%",
-                     buttons=[{"label": "A Website", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}],
+                     buttons=[{"label": "Get This Yourself", "url": "https://github.com/ApplePi313/Discord-RPC"}, {"label": "A Website", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}],
                      start=psutil.boot_time(),
                      state="Kernel: " + os.uname().release))  # Set the presence
     else:
         print(RPC.update(large_image="arch-linux", large_text="Arch Linux",
                      small_image="nvidia", small_text="nVidia RTX 3060 Laptop GPU",
                      details="RAM: " + str(mem_per) + "%  CPU: " + str(cpu_per) + "%",
-                     buttons=[{"label": "A Website", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}],
+                     buttons=[{"label": "Get This Yourself", "url": "https://github.com/ApplePi313/Discord-RPC"}, {"label": "A Website", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}],
                      start=psutil.boot_time(),
                      state="OS: " + os.uname().release))  # Set the presence
 
